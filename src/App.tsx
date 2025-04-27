e0a950ac4635086d66726f880b88f9fec478f3eb
bunchatests <bunchatests@gmail.com>
2025-04-28 00:44:03 +0500

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,51 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import twaLogo from "./assets/tapps.png";
-import viteLogo from "/vite.svg";
+import { useEffect, useRef } from "react";
 import "./App.css";
+import sampleNft from "./assets/nfts/yacht-club-monkey.png";
 
 import WebApp from "@twa-dev/sdk";
+import { useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
+import { NFTGrid } from "./components/NFTGrid";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [tonConnectUI] = useTonConnectUI();
+  const wallet = useTonWallet();
+
+  const walletRef = useRef(wallet);
+
+  useEffect(() => {
+    walletRef.current = wallet;
+    WebApp.MainButton.setText(wallet ? "Disconnect" : "Connect Wallet");
+  }, [wallet]);
+
+  useEffect(() => {
+    WebApp.MainButton.onClick(() => {
+      if (walletRef.current) {
+        console.log("'Disconnect' was clicked");
+        tonConnectUI.disconnect();
+      } else {
+        console.log("'Connect Wallet' was clicked");
+        tonConnectUI.openModal();
+      }
+    });
+    WebApp.MainButton.show();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TWA + Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      {/*  */}
-      <div className="card">
-        <button
-          onClick={() =>
-            WebApp.showAlert(`Hello World! Current count is ${count}`)
-          }
-        >
-          Show Alert
-        </button>
-      </div>
+      <main className="main">
+        <NFTGrid
+          data={Array.from({ length: 5 }, (_, i) => ({
+            id: i + 1,
+            image: sampleNft,
+          }))}
+        />
+        <NFTGrid
+          data={Array.from({ length: 5 }, (_, i) => ({
+            id: i + 1,
+            image: sampleNft,
+          }))}
+        />
+      </main>
     </>
   );
 }
